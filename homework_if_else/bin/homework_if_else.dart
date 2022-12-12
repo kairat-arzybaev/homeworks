031751d967b4f05c8fc7450fb41e409379811be6
kairat-arzybaev <66504325+kairat-arzybaev@users.noreply.github.com>
2022-12-12 15:20:14 +0600

    Added 'Число $num -'

--- homework_if_else/bin/homework_if_else.dart
+++ homework_if_else/bin/homework_if_else.dart
@@ -15,39 +15,39 @@ void main(List<String> arguments) {
   int income = Random().nextInt(490001) + 10000;
 
   if (income <= 100000) {
-    print('Процент: 5%, Сумма: ${income * 0.05}');
+    print('Процент: 5%\nСумма: ${income * 0.05}');
   } else if (income > 100000 && income <= 200000) {
     print('Процент: 7%, Сумма: ${income * 0.07}');
   } else {
-    print('Процент: 10%, Сумма: ${income * 0.1}');
+    print('Процент: 10%, Сумма: ${income * 0.07}');
   }
 
   // Задание 3
   int number = Random().nextInt(12) + 1;
 
   if (number == 1) {
-    print('Январь, Зима');
+    print('Число $number - Январь, Зима');
   } else if (number == 2) {
-    print('Февраль, Зима');
+    print('Число $number - Февраль, Зима');
   } else if (number == 3) {
-    print('Март, Весна');
+    print('Число $number - Март, Весна');
   } else if (number == 4) {
-    print('Апрель, Весна');
+    print('Число $number - Апрель, Весна');
   } else if (number == 5) {
-    print('Май, Весна');
+    print('Число $number - Май, Весна');
   } else if (number == 6) {
-    print('Июнь, Лето');
+    print('Число $number - Июнь, Лето');
   } else if (number == 7) {
-    print('Июль, Лето');
+    print('Число $number - Июль, Лето');
   } else if (number == 8) {
-    print('Август, Лето');
+    print('Число $number - Август, Лето');
   } else if (number == 9) {
-    print('Сентябрь, Осень');
+    print('Число $number - Сентябрь, Осень');
   } else if (number == 10) {
-    print('Октябрь, Осень');
+    print('Число $number - Октябрь, Осень');
   } else if (number == 11) {
-    print('Ноябрь, Осень');
+    print('Число $number - Ноябрь, Осень');
   } else {
-    print('Декабрь, Зима');
+    print('Число $number - Декабрь, Зима');
   }
 }
